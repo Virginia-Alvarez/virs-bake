3896dcf89782c80ee9f0b7689e72a2837cde8016
Virginia Álvarez <virginiaalvarez.ar@gmail.com>
2025-02-05 16:24:45 +0100

    add a new skeleton for fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
-// import { StrictMode} from 'react';
-// import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
-import App from './App.jsx';
-import ReactDOM from 'react-dom/client';
-import {HashRouter} from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <HashRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </HashRouter>
-);
-
+  </StrictMode>,
+)
